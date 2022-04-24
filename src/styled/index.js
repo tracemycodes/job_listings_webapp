881c6738b44578d6f68d86b126604fd4562c5460
Agba Emmanuel <agbaayoh@gmail.com>
2022-04-24 08:21:43 -0700

    job listing project made fully reponsive

--- src/styled/index.js
+++ src/styled/index.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
-import headerImg from '../components/images/bg-header-desktop.svg'
-
-
+import headerImg from '../components/images/bg-header-desktop.svg';
 
 export const HeaderBanner = styled.header`
   height: 9rem;
@@ -23,35 +21,35 @@ export const FilterDiv = styled.article`
   margin-top: -3.5rem;
   margin-bottom: 2rem;
   background-color: white;
-  box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
+  box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
 
   p {
-      margin-left: auto;
-      color: hsl(180, 14%, 20%);
-      cursor: pointer;
-      font-size: clamp(.75rem, 2.5vw, 1rem);
-      &:hover {
-        color: hsl(180, 29%, 50%);
-      }
+    margin-left: auto;
+    color: hsl(180, 14%, 20%);
+    cursor: pointer;
+    font-size: clamp(0.75rem, 2.5vw, 1rem);
+    &:hover {
+      color: hsl(180, 29%, 50%);
     }
+  }
 
   button {
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding-left: .4rem;
+    padding-left: 0.4rem;
     border: none;
-    margin-top: .5rem;
-    margin-bottom: .5rem;
-    margin-right: clamp(.6rem, 2vw, 1rem);
-    font-size: clamp(.6rem, 2vw, .75rem);
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+    margin-right: clamp(0.6rem, 2vw, 1rem);
+    font-size: clamp(0.6rem, 2vw, 0.75rem);
     font-weight: bold;
     color: hsl(180, 29%, 50%);
     border-radius: 4px;
 
     img {
-      padding: clamp(.1rem, 1vw, .3rem);;
-      margin-left: .4rem;
+      padding: clamp(0.1rem, 1vw, 0.3rem);
+      margin-left: 0.4rem;
       background-color: hsl(180, 29%, 50%);
       border-top-right-radius: 4px;
       border-bottom-right-radius: 4px;
@@ -61,10 +59,8 @@ export const FilterDiv = styled.article`
         background-color: hsl(180, 14%, 20%);
       }
     }
-    
   }
-
-`
+`;
 
 export const Container = styled.section`
   background-color: hsl(180, 52%, 96%);
@@ -76,30 +72,42 @@ export const JobDiv = styled.article`
   max-width: 60rem;
   margin: auto;
   background-color: white;
-  padding: 1rem 2rem;
   border-radius: 4px;
+  position: relative;
+  padding: 1rem 2rem;
   margin-bottom: 1.5rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
+  box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
 
   @media (max-width: 790px) {
-    border: 2px solid blue;
-    padding: 1rem 1rem;
+    padding: 0.8rem 0.8rem;
+  }
+  @media (max-width: 730px) {
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 2rem 1.5rem 1.5rem 1.5rem;
+    margin: 2rem auto 3.5rem;
+    max-width: 30rem;
+    width: 90%;
   }
 
   & .company-details {
     display: flex;
+    position: relative;
   }
   .company-logo {
     margin-right: 1rem;
-    // width: 5rem;
     width: clamp(3.5rem, 5vw, 5rem);
     height: clamp(3.5rem, 5vw, 5rem);
-    // height: 5rem;
+
+    @media (max-width: 730px) {
+      position: absolute;
+      top: -4rem;
+    }
   }
-  .company-logo img{
+  .company-logo img {
     width: 100%;
     height: 100%;
     object-fit: cover;
@@ -110,55 +118,76 @@ export const JobDiv = styled.article`
     justify-content: space-between;
   }
   h2 {
-    
-    font-size: clamp(.75rem, 3vw, 1rem);
+    font-size: clamp(0.75rem, 3vw, 1rem);
     color: hsl(180, 14%, 20%);
+     @media (max-width: 730px) {
+    
+    margin: .25rem 0;
+  }
   }
   h3 {
-    font-size: clamp(.75rem, 3vw, 1rem);
+    font-size: clamp(0.75rem, 3vw, 1rem);
     color: hsl(180, 29%, 50%);
     font-weight: bold;
     display: inline;
   }
   .featured {
     background-color: hsl(180, 14%, 20%);
-    padding: .2rem .4rem;
+    padding: 0.2rem 0.4rem;
     color: white;
     border-radius: 10px;
     font-weight: bold;
-    font-size: clamp(.6rem, 2vw, .75rem);
+    font-size: clamp(0.6rem, 2vw, 0.75rem);
+    @media (max-width: 730px) {
+    
+    font-size: clamp(0.6rem, 2vw, 0.45rem);
+  }
   }
   .new {
     background-color: hsl(180, 29%, 50%);
-    padding: .2rem .4rem;
+    padding: 0.2rem 0.4rem;
     color: white;
     border-radius: 10px;
-    margin: 0 .75rem;
+    margin: 0 0.75rem;
     font-weight: bold;
-    font-size: clamp(.6rem, 2.5vw, .75rem);
+    font-size: clamp(0.6rem, 2.5vw, 0.75rem);
+     @media (max-width: 730px) {
+    
+    font-size: clamp(0.6rem, 2vw, 0.45rem);
+  }
   }
   .company-duration span {
-    font-size: clamp(.6rem, 2.5vw, .75rem);
+    font-size: clamp(0.6rem, 2.5vw, 0.75rem);
     color: hsl(180, 8%, 52%);
   }
-  .company-duration *+* {
+  .company-duration * + * {
     margin-left: 1rem;
   }
   .company-roles {
-    border: 2px solid green;
+
+     @media (max-width: 730px) {
+    border-top: 1px solid #b5b69c;
+    padding-top: 1rem;
+    margin-top: 1rem;
+    width: 100%;
+  }
   }
   .company-roles button {
-    margin-left: 1rem;
     margin-bottom: 1rem;
     background-color: hsl(180, 52%, 96%);
     border: none;
-    padding: .3rem .5rem;
+    padding: 0.3rem 0.5rem;
     border-radius: 3px;
     color: hsl(180, 29%, 50%);
-    font-size: clamp(.6rem, 2vw, .75rem);
+    font-size: clamp(0.6rem, 2vw, 0.75rem);
     font-weight: bold;
-    box-shadow: 0px 5px 5px -3px rgba(0,0,0,0.1);
+    box-shadow: 0px 5px 5px -3px rgba(0, 0, 0, 0.1);
     cursor: pointer;
     float: right;
+     @media (max-width: 730px) {
+    margin-right: 1rem;
+    float: unset;
   }
-`
+  
+  }
+`;
